6b06b8bcd0159000d87dfcf58dbc28b462725e87
Sufeng Niu <sniu@g.clemson.edu>
2015-11-08 12:24:31 -0500

    update preprocess, which can load tsv file

---
+++ scripts/preprocess.py
@@ -0,0 +1,108 @@
+import csv
+import mmap
+import progressbar
+import wikipedia
+
+
+
+# with open('../data/training_set.tsv', 'rb') as csvfile:
+# 	readDict = csv.DictReader(csvfile, delimiter='\t', quotechar=' ')
+# 	parseDict = {}
+# 	# for row in readDict:
+# 	# 	print ',' jion(row)
+# 	for line in readDict:
+# 		ID = line['id'].strip()
+# 		QUESTION = line['question'].strip()
+# 		QUESTION_WORD = QUESTION.split()
+# 		CORRECTANSWER = line['correctAnswer'].strip()
+# 		CORRECTANSWER_WORD = CORRECTANSWER.split()
+# 		A = line['A'].strip()
+# 		A_WORD = A.split()
+# 		B = line['B'].strip()
+# 		B_WORD = B.split()
+# 		C = line['C'].strip()
+# 		C_WORD = C.split()
+# 		D = line['D'].strip()
+# 		D_WORD = D.split()
+# 		parseDict[ID] = {}
+# 		parseDict[ID]['question'] = QUESTION_WORD
+# 		parseDict[ID]['correctAnswer'] = CORRECTANSWER_WORD
+# 		parseDict[ID]['A'] = A_WORD
+# 		parseDict[ID]['B'] = B_WORD
+# 		parseDict[ID]['C'] = C_WORD
+# 		parseDict[ID]['D'] = D_WORD
+
+parseDict = {}
+
+
+
+
+class preprocess(object):
+	""" preprocess class apply load file and parsing """
+
+	def __init__(self, input_file):
+		self.readDict = {}	
+		if input_file.endswith('.tsv'):		# tsv file
+			self.parse_tsv(input_file)
+		# elif		# TODO: wiki dump file
+		else:
+			sys.exit("incorrect input file format, must be tsv (.tsv) and wikipedia dump files (.xml)")
+
+	def load_wiki(self, input_file):
+		""" read wiki pre-train file """
+		print 'load wiki dump file (', input_file, ')...'
+		# TODO
+		self.wiki_parse()
+		pass
+
+	def parse_tsv(self, input_file):	
+		""" read input tsv file and parse input file by \t.
+			format each question as following:
+			question id: use id to query each question contents
+			question contents
+			answer (A/B/C/D)
+			selection A
+			selection B
+			selection C
+			selection D
+		"""	
+		print 'load tsv input file (', input_file, ')...'
+		with open(input_file, 'rb') as csvfile:
+			self.readDict = csv.DictReader(csvfile, delimiter='\t', quotechar=' ')
+			lineNum = sum(1 for line in self.readDict)	
+			bar = progressbar.ProgressBar(maxval = lineNum, \
+					widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+			print 'load file done, parse the input'
+			i = 0
+			for line in self.readDict:
+				bar.update(i+1)
+				csv.DictReader(csvfile, delimiter='\t', quotechar=' ')		
+				ID = line['id'].strip()
+				QUESTION = line['question'].strip()
+				QUESTION_WORD = QUESTION.split()
+				CORRECTANSWER = line['correctAnswer'].strip()
+				CORRECTANSWER_WORD = CORRECTANSWER.split()
+				A = line['A'].strip()
+				A_WORD = A.split()
+				B = line['B'].strip()
+				B_WORD = B.split()
+				C = line['C'].strip()
+				C_WORD = C.split()
+				D = line['D'].strip()
+				D_WORD = D.split()
+
+				parseDict[ID] = {}
+				parseDict[ID]['question'] = QUESTION_WORD
+				parseDict[ID]['correctAnswer'] = CORRECTANSWER_WORD
+				parseDict[ID]['A'] = A_WORD
+				parseDict[ID]['B'] = B_WORD
+				parseDict[ID]['C'] = C_WORD
+				parseDict[ID]['D'] = D_WORD
+			bar.finish()
+		print 'parsing done'
+		
+
+# initial test
+
+preprocess('../data/training_set.tsv')
+
