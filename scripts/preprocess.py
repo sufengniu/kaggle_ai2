8edf38c6523af2cd0cd687a2f3f5570504d987fd
Sufeng Niu <sniu@g.clemson.edu>
2015-11-09 21:15:18 -0500

    preprocess can load wikipedia now

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -3,38 +3,14 @@ import mmap
 import progressbar
 import wikipedia
 
+# import os
+import subprocess
 
 
-# with open('../data/training_set.tsv', 'rb') as csvfile:
-# 	readDict = csv.DictReader(csvfile, delimiter='\t', quotechar=' ')
-# 	parseDict = {}
-# 	# for row in readDict:
-# 	# 	print ',' jion(row)
-# 	for line in readDict:
-# 		ID = line['id'].strip()
-# 		QUESTION = line['question'].strip()
-# 		QUESTION_WORD = QUESTION.split()
-# 		CORRECTANSWER = line['correctAnswer'].strip()
-# 		CORRECTANSWER_WORD = CORRECTANSWER.split()
-# 		A = line['A'].strip()
-# 		A_WORD = A.split()
-# 		B = line['B'].strip()
-# 		B_WORD = B.split()
-# 		C = line['C'].strip()
-# 		C_WORD = C.split()
-# 		D = line['D'].strip()
-# 		D_WORD = D.split()
-# 		parseDict[ID] = {}
-# 		parseDict[ID]['question'] = QUESTION_WORD
-# 		parseDict[ID]['correctAnswer'] = CORRECTANSWER_WORD
-# 		parseDict[ID]['A'] = A_WORD
-# 		parseDict[ID]['B'] = B_WORD
-# 		parseDict[ID]['C'] = C_WORD
-# 		parseDict[ID]['D'] = D_WORD
+
+
 
 parseDict = {}
-
-
 
 
 class preprocess(object):
@@ -44,16 +20,20 @@ class preprocess(object):
 		self.readDict = {}	
 		if input_file.endswith('.tsv'):		# tsv file
 			self.parse_tsv(input_file)
-		# elif		# TODO: wiki dump file
+		elif input_file.endswith('.xml'):	# wiki dump file (.xml)
+			self.load_wiki(input_file)
 		else:
 			sys.exit("incorrect input file format, must be tsv (.tsv) and wikipedia dump files (.xml)")
 
 	def load_wiki(self, input_file):
 		""" read wiki pre-train file """
 		print 'load wiki dump file (', input_file, ')...'
-		# TODO
-		self.wiki_parse()
-		pass
+		
+		# os.system('../utils/wikiextractor/WikiExtractor "%s"' % input_file)
+		subprocess.call("../utils/wikiextractor/WikiExtractor.py " + input_file, shell=True)
+		
+		# self.wiki_parse()
+
 
 	def parse_tsv(self, input_file):	
 		""" read input tsv file and parse input file by \t.
@@ -105,4 +85,5 @@ class preprocess(object):
 # initial test
 
 preprocess('../data/training_set.tsv')
+preprocess('../data/wikipedia_sci/wiki_sample.xml')
 
