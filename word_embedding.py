c2c39531823ce10768ba674b720e6a90846bdf7d
Sufeng Niu <sniu@g.clemson.edu>
2016-01-06 14:36:27 -0500

    still tune word2vec accuracy

--- word_embedding.py
+++ word_embedding.py
@@ -14,38 +14,47 @@ import numpy as np
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
-class my_sentences(object):
-	def __init__(self, dirname):
+class dir_sentences(object):
+	def __init__(self, dirname):	
 		self.dirname = dirname
-	
+
 	def __iter__(self):
-		for fname in os.listdir(self.dirname):
-			for line in open(os.path.join(self.dirname, fname)):
+		for filename in os.listdir(self.dirname):
+			for line in open(os.path.join(self.dirname, filename)):
 				yield line.split()
 
-class model_train(object):
-    """ word2vec """
+class w2v_model(object):
+    """ word2vec, doc2vec """
     def __init__(self):
     	self.model = Word2Vec()
 
-    def word2vec(self, sentences):
-    	# ignore words less then 5 times, parallel threads to be 8, 100 neuron size (50 to 100)
-    	self.model = Word2Vec(sentences, min_count=5, size=200, window=5, workers=8)
+    def train(self, sentences):
+    	# ignore words less then 5 times, parallel threads to be 8, 100 neuron size (50 to 100)	
+		bigram_transformer = gensim.models.Phrases(sentences)
+		self.model = Word2Vec(bigram_transformer[sentences], min_count=5, size=100, window=5, workers=8)
+		self.model.save('models/mymodel')
+		#self.model.init_sims(replace=True)
 		#self.model.build_vocab()
 
-    def train(self):
-        pass
-
-    def word2phrase(self):
-        pass
+    def ctrain(self, sentences):
+		self.model = Word2Vec.load('models/mymodel')
+		self.model.train(sentences)
 
     def accuracy(self):
 		self.model.accuracy('data/questions-words.txt')
 
-wiki = my_sentences('data/wikipedia_sci')
-w2v_model = model_train()
-w2v_model.word2vec(sentences)
-w2v_model.accuracy()
+# merge wiki data with text8
+wiki = dir_sentences('data/wikipedia_sci')
+text8 = LineSentence('data/text8')
+
+pre_model = w2v_model()
+pre_model.train(text8)
+pre_model.accuracy()
+
+pre_model.ctrain(wiki)
+pre_model.accuracy()
+
+
 
 # load pre-train model from GloVe
 
